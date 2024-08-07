1db2a4091afe7335d864405d2b735296b29543ec
amberw95 <amberw95@gmail.com>
2024-08-07 14:35:05 -0400

    chapter 13

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,10 +1,13 @@
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import Form from '@/app/ui/invoices/edit-form';
 import { fetchCustomers, fetchInvoiceById } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page( { params }: { params: {id: string}} ) {
     const id = params.id;
     const [customers, invoice] = await Promise.all([fetchCustomers(), fetchInvoiceById(id)]);
+
+    if(!invoice) return notFound();
     return (
         <main>
             <Breadcrumbs
